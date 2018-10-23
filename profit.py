79cf1bd0e04fd4507a6cfe4778f5bef4e6df9600
Mark Schmidt <schmidtwmark@gmail.com>
2018-10-22 19:40:48 -0500

    O(n) solution

--- profit.py
+++ profit.py
@@ -23,8 +23,28 @@ def MP(k):
             Cache[i][l] = max(skip, build)
     print(Cache)
 
-MP(3)
+def MP_2(k):
+    Cache = np.zeros((n+1, 2))
+    for l in range(1, k+1):
+        for i in range(n-1, -1, -1):
+            temp = Next(i) 
+            if l % 2 == 1:
+                skip = Cache[i+1][1]
+                build = Profits[i] + Cache[temp][0]
+                Cache[i][1] = max(skip, build)
+            else:
+                skip = Cache[i+1][0]
+                build = Profits[i] + Cache[temp][1]
+                Cache[i][0] = max(skip, build)
+    print(Cache)
 
+    if k % 2 is 1:
+        return Cache[0,1]
+    else:
+        return Cache[0,0]
+
+MP(2)
+print(MP_2(2))
 
 
 
